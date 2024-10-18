f7ae7e2cb37160acb348435dd16a86d036a9cd0b
Mohammad Tarequl islam <tareq_au@hotmail.com>
2024-10-19 10:26:24 +1100

    Update code.sql

--- code.sql
+++ code.sql
@@ -1,4 +1,10 @@
 
+Q3. Show patient_id and first_name from patients where their first_name start and ends with 's' and is at least 6 characters long.
+  
+  SELECT patient_id, first_name FROM patients
+where first_name like 's%s'and len(first_name)>=6
+  
+    
 Q2. Show unique first names from the patients table which only occurs once in the list.
 For example, if two or more people are named 'John' in the first_name column then don't include their name in the output list. If only 1 person is named 'Leo' then include them in the output.
                         
