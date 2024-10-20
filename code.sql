3844b3466c15283541515f6739ab80c8c5ee9ae4
Mohammad Tarequl islam <tareq_au@hotmail.com>
2024-10-20 11:12:50 +1100

    Update code.sql

--- code.sql
+++ code.sql
@@ -1,4 +1,12 @@
+Q4. Show patient_id, first_name, last_name from patients whos diagnosis is 'Dementia'.
+Primary diagnosis is stored in the admissions table.
 
+  SELECT p.patient_id,p.first_name, p.last_name
+FROM patients p 
+	join admissions a
+on p.patient_id=a.patient_id
+where diagnosis ="Dementia"
+  
 Q3. Show patient_id and first_name from patients where their first_name start and ends with 's' and is at least 6 characters long.
   
   SELECT patient_id, first_name FROM patients
